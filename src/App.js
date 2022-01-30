9bf4bc5c78df06db997065ba1706c7feff9ded12
carlosqdev <carlosquintero95nic@gmail.com>
2022-01-29 18:35:10 -0600

    Feat: I added the components Form.jsx, AllProducts.jsx, Product.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import AllProducts from "./components/AllProducts";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container" style={{height: "100vh"}}>
+      <div className="row">
+        <Form />
+        <AllProducts />
+      </div>
+    </section>
   );
 }
 
