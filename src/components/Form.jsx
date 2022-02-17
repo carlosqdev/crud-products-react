b868868fb6f8572cb3033d8021aac5ca2aad337f
carlosqdev <carlosquintero95nic@gmail.com>
2022-02-16 20:48:04 -0600

    Feat: Form.jsx is already functional

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-function Form() {
+const initialState = {
+  cod: "",
+  description: "",
+  price: "",
+};
+
+function Form({ addOrUpdateProduct, currentCode, allProducts }) {
+  const [product, setProduct] = useState(initialState);
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addOrUpdateProduct(product);
+    setProduct(initialState);
+  };
+
+  useEffect(() => {
+    if (currentCode > 0) {
+      let currentProduct = allProducts.filter(
+        (product) => product.cod === currentCode
+      );
+      setProduct(currentProduct[0]);
+    }
+  }, [currentCode]);
+
   return (
-    <>
-      <h1 className='text-center mt-2'>CRUD</h1>
-      <form className='col-md-4'>
-        <div className="form-group">
-          <label className="form-label" htmlFor="cod">Codigo</label>
-          <input className="form-control" type="text" name="cod" id="cod" placeholder='Escribe código del producto'/>
-        </div>
+    <form onSubmit={handleSubmit} className="col-md-4">
+      <div className="form-group">
+        <label className="form-label" htmlFor="cod">
+          Codigo
+        </label>
+        <input
+          onChange={handleChangeInput}
+          value={product.cod}
+          className="form-control"
+          type="text"
+          name="cod"
+          id="cod"
+          placeholder="Escribe código del producto"
+          readOnly={currentCode > 0 ? true : false}
+          required
+        />
+      </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="description">Descripción</label>
-          <input className="form-control" type="text" name="description" id="description" placeholder='Describe el producto' />
-        </div>
+      <div className="form-group">
+        <label className="form-label" htmlFor="description">
+          Descripción
+        </label>
+        <input
+          onChange={handleChangeInput}
+          value={product.description}
+          className="form-control"
+          type="text"
+          name="description"
+          id="description"
+          placeholder="Describe el producto"
+          required
+        />
+      </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="price">Precio</label>
-          <input className="form-control" type="text" name="price" id="price" placeholder='Precio del producto' />
-        </div>
+      <div className="form-group">
+        <label className="form-label" htmlFor="price">
+          Precio
+        </label>
+        <input
+          onChange={handleChangeInput}
+          value={product.price}
+          className="form-control"
+          type="text"
+          name="price"
+          id="price"
+          placeholder="Precio del producto"
+          required
+        />
+      </div>
 
-        <div className="d-grid gap-2 mt-3">
-          <button className="btn btn-lg btn-primary" type="submit">Guardar</button>
-        </div>
-      </form>
-    </>
+      <div className="d-grid gap-2 mt-3">
+        <button className="btn btn-lg btn-primary" type="submit">
+          {currentCode > 0 ? "Actualizar" : "Guardar"}
+        </button>
+      </div>
+    </form>
   );
 }
 
