336209604d3b29771f8656edc0bb09e433b3056c
carlosqdev <carlosquintero95nic@gmail.com>
2022-02-16 20:16:18 -0600

    Feat: src/App.js
    I added states allProducts, currentProduct
    
    I added functions: addOrUpdateProduct, deleteProduct
    
    Add props to Form, AllProducts components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import React, { useState } from "react";
 import AllProducts from "./components/AllProducts";
 import Form from "./components/Form";
 
+const initialState = []
+
 function App() {
+  const [allProducts, setAllProducts] = useState(initialState)
+  const [currentCode, setCurrentCode] = useState(0)
+
+  const addOrUpdateProduct = (dataProduct) => {
+    if(currentCode > 0) {
+      const exist = allProducts.find((product) => product.cod === currentCode)
+      if(exist) {
+        setAllProducts(
+          allProducts.map((product) => product.cod === currentCode ? {...exist, description: dataProduct.description, price: dataProduct.price} : product)
+        )
+        setCurrentCode(0)
+      }
+    } else {
+      setAllProducts([...allProducts, {...dataProduct}])
+    }
+  }
+
+  const deleteProduct = (codeProduct) => {
+    setAllProducts(allProducts.filter(product => product.cod !== codeProduct))
+  }
+
   return (
-    <section className="container" style={{height: "100vh"}}>
+    <section className="container mt-5" style={{height: "100vh"}}>
       <div className="row">
-        <Form />
-        <AllProducts />
+        <Form
+          addOrUpdateProduct={addOrUpdateProduct}
+          currentCode={currentCode}
+          allProducts={allProducts}
+        />
+        <AllProducts
+          allProducts={allProducts}
+          deleteProduct={deleteProduct}
+          currentCode={currentCode}
+          setCurrentCode={setCurrentCode}
+        />
       </div>
     </section>
   );
