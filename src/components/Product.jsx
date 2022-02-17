c03e8b883d06e2f49e1a210d23a07e83afce035a
carlosqdev <carlosquintero95nic@gmail.com>
2022-02-16 20:50:08 -0600

    Feat: Product.jsx is functional

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
-function Product() {
+function Product({cod, description, price, deleteProduct, currentCode, setCurrentCode}) {
   return (
     <section style={{maxWidth: "20rem"}} className="card border-primary mb-3">
       <header className="card-header">
         <h3>Información del producto</h3>
       </header>
       <div className="card-body">
-        <p className="card-text">Codigo: 11212</p>
-        <p className="card-text">Descripción: Camiseta color azul</p>
-        <p className="card-text">Precio: $20.00</p>
+        <p className="card-text">Codigo: {cod}</p>
+        <p className="card-text">Descripción: {description}</p>
+        <p className="card-text">Precio: ${price}</p>
       </div>
       <div style={{padding: "1rem"}} className="d-grid gap-2 mb-2">
-        <button className="btn btn-secondary">Editar</button>
-        <button className="btn btn-danger">Eliminar</button>
+        <button
+          onClick={() => setCurrentCode(cod)}
+          className="btn btn-secondary">Editar</button>
+
+        {currentCode <= 0 && (
+          <button
+            onClick={() => deleteProduct(cod)}
+            className="btn btn-danger">Eliminar</button>
+        )}
       </div>
     </section>
   );
