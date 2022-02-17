cdd5e313a6d2648919e0290d9c542414867974ee
carlosqdev <carlosquintero95nic@gmail.com>
2022-02-16 20:49:02 -0600

    Feat: AllProducts.jsx is functional

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Product from "./Product"
 
-function AllProducts() {
+function AllProducts({allProducts, deleteProduct, currentCode, setCurrentCode}) {
   return (
     <section className="col-md-8">
       <div style={{display: "flex", flexWrap: "wrap", gap: "10px", justifyContent: "space-between"}}>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {allProducts?.map(product => (
+          <Product
+            key={product.cod}
+            cod={product.cod}
+            description={product.description}
+            price={product.price}
+            deleteProduct={deleteProduct}
+            currentCode={currentCode}
+            setCurrentCode={setCurrentCode}
+          />
+        ))}
       </div>
     </section>
   );
